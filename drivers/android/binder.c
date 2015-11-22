6c8005887ce3454d11551912ebab84bf74c62d03
Dorimanx <yuri@bynet.co.il>
2015-11-22 13:01:32 +0200

    android: binder: Disable preemption while freeing kernel memory.
    This reduces contention on the global binder lock by removing
    the preemption points within the free path in the kernel.
    
    Change-Id: Id0ca19eedb70eb94a1308ebfca8774620ffc23e1
    
    https://github.com/myfluxi/android_kernel_lge_hammerhead/commit/612a18b5d54be5820b1ed648a2ab78d42d8b6471

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -440,6 +440,13 @@ static void __set_user_nice_no_resched(long nice)
 	sched_preempt_enable_no_resched();
 }
 
+static void kfree_no_resched(const void *objp)
+{
+	preempt_disable();
+	kfree(objp);
+	preempt_enable_no_resched();
+}
+
 static void binder_set_nice(long nice)
 {
 	long min_nice;
@@ -997,7 +1004,7 @@ static int binder_dec_node(struct binder_node *node, int strong, int internal)
 					     "dead node %d deleted\n",
 					     node->debug_id);
 			}
-			kfree(node);
+			kfree_no_resched(node);
 			binder_stats_deleted(BINDER_STAT_NODE);
 		}
 	}
@@ -1113,10 +1120,10 @@ static void binder_delete_ref(struct binder_ref *ref)
 			     "%d delete ref %d desc %d has death notification\n",
 			      ref->proc->pid, ref->debug_id, ref->desc);
 		list_del(&ref->death->work.entry);
-		kfree(ref->death);
+		kfree_no_resched(ref->death);
 		binder_stats_deleted(BINDER_STAT_DEATH);
 	}
-	kfree(ref);
+	kfree_no_resched(ref);
 	binder_stats_deleted(BINDER_STAT_REF);
 }
 
@@ -1191,7 +1198,7 @@ static void binder_pop_transaction(struct binder_thread *target_thread,
 	t->need_reply = 0;
 	if (t->buffer)
 		t->buffer->transaction = NULL;
-	kfree(t);
+	kfree_no_resched(t);
 	binder_stats_deleted(BINDER_STAT_TRANSACTION);
 }
 
@@ -1746,10 +1753,10 @@ err_copy_data_failed:
 	t->buffer->transaction = NULL;
 	binder_free_buf(target_proc, t->buffer);
 err_binder_alloc_buf_failed:
-	kfree(tcomplete);
+	kfree_no_resched(tcomplete);
 	binder_stats_deleted(BINDER_STAT_TRANSACTION_COMPLETE);
 err_alloc_tcomplete_failed:
-	kfree(t);
+	kfree_no_resched(t);
 	binder_stats_deleted(BINDER_STAT_TRANSACTION);
 err_alloc_t_failed:
 err_bad_call_stack:
@@ -2296,7 +2303,7 @@ retry:
 				     proc->pid, thread->pid);
 
 			list_del(&w->entry);
-			kfree(w);
+			kfree_no_resched(w);
 			binder_stats_deleted(BINDER_STAT_TRANSACTION_COMPLETE);
 		} break;
 		case BINDER_WORK_NODE: {
@@ -2356,7 +2363,7 @@ retry:
 						     (u64)node->ptr,
 						     (u64)node->cookie);
 					rb_erase(&node->rb_node, &proc->nodes);
-					kfree(node);
+					kfree_no_resched(node);
 					binder_stats_deleted(BINDER_STAT_NODE);
 				} else {
 					binder_debug(BINDER_DEBUG_INTERNAL_REFS,
@@ -2397,7 +2404,7 @@ retry:
 
 			if (w->type == BINDER_WORK_CLEAR_DEATH_NOTIFICATION) {
 				list_del(&w->entry);
-				kfree(death);
+				kfree_no_resched(death);
 				binder_stats_deleted(BINDER_STAT_DEATH);
 			} else
 				list_move(&w->entry, &proc->delivered_death);
@@ -2477,7 +2484,7 @@ retry:
 			thread->transaction_stack = t;
 		} else {
 			t->buffer->transaction = NULL;
-			kfree(t);
+			kfree_no_resched(t);
 			binder_stats_deleted(BINDER_STAT_TRANSACTION);
 		}
 		break;
@@ -2529,7 +2536,7 @@ static void binder_release_work(struct list_head *list)
 		case BINDER_WORK_TRANSACTION_COMPLETE: {
 			binder_debug(BINDER_DEBUG_DEAD_TRANSACTION,
 				"undelivered TRANSACTION_COMPLETE\n");
-			kfree(w);
+			kfree_no_resched(w);
 			binder_stats_deleted(BINDER_STAT_TRANSACTION_COMPLETE);
 		} break;
 		case BINDER_WORK_DEAD_BINDER_AND_CLEAR:
@@ -2540,7 +2547,7 @@ static void binder_release_work(struct list_head *list)
 			binder_debug(BINDER_DEBUG_DEAD_TRANSACTION,
 				"undelivered death notification, %016llx\n",
 				(u64)death->cookie);
-			kfree(death);
+			kfree_no_resched(death);
 			binder_stats_deleted(BINDER_STAT_DEATH);
 		} break;
 		default:
@@ -2623,7 +2630,7 @@ static int binder_free_thread(struct binder_proc *proc,
 	if (send_reply)
 		binder_send_failed_reply(send_reply, BR_DEAD_REPLY);
 	binder_release_work(&thread->todo);
-	kfree(thread);
+	kfree_no_resched(thread);
 	binder_stats_deleted(BINDER_STAT_THREAD);
 	return active_transactions;
 }
@@ -2961,7 +2968,7 @@ static int binder_mmap(struct file *filp, struct vm_area_struct *vma)
 	return 0;
 
 err_alloc_small_buf_failed:
-	kfree(proc->pages);
+	kfree_no_resched(proc->pages);
 	proc->pages = NULL;
 err_alloc_pages_failed:
 	mutex_lock(&binder_mmap_lock);
@@ -3062,7 +3069,7 @@ static int binder_node_release(struct binder_node *node, int refs)
 	binder_release_work(&node->async_todo);
 
 	if (hlist_empty(&node->refs)) {
-		kfree(node);
+		kfree_no_resched(node);
 		binder_stats_deleted(BINDER_STAT_NODE);
 
 		return refs;
@@ -3188,7 +3195,7 @@ static void binder_deferred_release(struct binder_proc *proc)
 			__free_page(proc->pages[i]);
 			page_count++;
 		}
-		kfree(proc->pages);
+		kfree_no_resched(proc->pages);
 		vfree(proc->buffer);
 	}
 
@@ -3199,7 +3206,7 @@ static void binder_deferred_release(struct binder_proc *proc)
 		     __func__, proc->pid, threads, nodes, incoming_refs,
 		     outgoing_refs, active_transactions, buffers, page_count);
 
-	kfree(proc);
+	kfree_no_resched(proc);
 }
 
 static void binder_deferred_func(struct work_struct *work)
