3e51a0a55aa9eca7c4d16e879751cf91bd1e6744
flar2 <asegaert@gmail.com>
2016-06-28 22:04:08 -0400

    otg: enable switch
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -252,7 +252,7 @@ struct dwc3_msm {
 bool check_P3_ready = false;
 
 bool gadget_start = false;
-int otg_switch;
+int otg_switch = 1;
 struct dwc3_msm *opmdwc;
 
 static  int oem_test_id(int nr, const volatile unsigned long *addr, enum usb_otg_state otg_state)
