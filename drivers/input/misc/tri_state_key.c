223a3e94f3349f713f9ee37c0a72533be48c80a4
engstk <eng.stk@sapo.pt>
2016-11-11 10:50:36 +0000

    input: tri_state_key: Fix direct references to HZ
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/misc/tri_state_key.c
+++ drivers/input/misc/tri_state_key.c
@@ -124,7 +124,7 @@ irqreturn_t switch_dev_interrupt(int irq, void *_dev)
 
 static void timer_handle(unsigned long arg)
 {
-    //mod_timer(&s_timer, jiffies + HZ);
+    //mod_timer(&s_timer, jiffies + msecs_to_jiffies(1000));
   //  if(set_gpio_by_pinctrl() < 0)
    //      printk(KERN_ERR "tristate_key set_gpio_by_pinctrl FAILD!!!. \n");
     schedule_work(&switch_data->work);
@@ -442,7 +442,7 @@ static int tristate_dev_probe(struct platform_device *pdev)
 
         init_timer(&switch_data->s_timer);
         switch_data->s_timer.function = &timer_handle;
-        switch_data->s_timer.expires = jiffies + 5*HZ;
+        switch_data->s_timer.expires = jiffies + msecs_to_jiffies(5000);
 
         add_timer(&switch_data->s_timer);
 
