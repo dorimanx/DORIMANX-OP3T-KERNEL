d9c0037599e62982631aadcce9bce353ff6871b3
Sahitya Tummala <stummala@codeaurora.org>
2016-11-30 09:00:53 +0530

    mmc: block: Fix issue with deferred resume when CQ is enabled
    In CQ mode, the legacy commands will be sent after CQ is
    put in halt state. But when deferred resume is enabled,
    the resume happens in mmc_wait_for_req(), which will enable
    CQ again overiding the caller's state. This causes legacy
    commands to fail. Fix this by moving the deferred resume
    from all the places to mmc_get_card() which is always the
    first step done in both CQ/legacy commands and thus avoids
    the above issue scneario.
    
    Change-Id: I432e89f3221615dd644412f481d17f840c75e407
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -3568,10 +3568,6 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 		/* claim host only for the first request */
 		mmc_get_card(card);
 
-#ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
-	if (mmc_bus_needs_resume(card->host))
-		mmc_resume_bus(card->host);
-#endif
 		if (mmc_card_doing_bkops(host->card)) {
 			ret = mmc_stop_bkops(host->card);
 			if (ret)
