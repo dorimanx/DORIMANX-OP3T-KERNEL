38e3b80bc1bd67ebae1f84b375d21b8836cfb9f2
Manoj Prabhu B <bmanoj@codeaurora.org>
2016-12-29 13:19:48 +0530

    memshare: Fix possible memory leak
    This patch frees up the dynamically allotted memory
    when met with error condition and function return.
    
    CRs-Fixed: 1106180
    Change-Id: I01446fbdbb422dcb025ee50929b32f40a5a78681
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- drivers/soc/qcom/memshare/msm_memshare.c
+++ drivers/soc/qcom/memshare/msm_memshare.c
@@ -480,6 +480,8 @@ static int handle_alloc_generic_req(void *req_h, void *req, void *conn_h)
 		pr_err("memshare: %s client not found, requested client: %d, proc_id: %d\n",
 				__func__, alloc_req->client_id,
 				alloc_req->proc_id);
+		kfree(alloc_resp);
+		alloc_resp = NULL;
 		return -EINVAL;
 	}
 
@@ -524,6 +526,9 @@ static int handle_alloc_generic_req(void *req_h, void *req, void *conn_h)
 	if (rc < 0)
 		pr_err("In %s, Error sending the alloc request: %d\n",
 							__func__, rc);
+
+	kfree(alloc_resp);
+	alloc_resp = NULL;
 	return rc;
 }
 
@@ -641,6 +646,8 @@ static int handle_query_size_req(void *req_h, void *req, void *conn_h)
 		pr_err("memshare: %s client not found, requested client: %d, proc_id: %d\n",
 				__func__, query_req->client_id,
 				query_req->proc_id);
+		kfree(query_resp);
+		query_resp = NULL;
 		return -EINVAL;
 	}
 
@@ -666,6 +673,8 @@ static int handle_query_size_req(void *req_h, void *req, void *conn_h)
 		pr_err("In %s, Error sending the query request: %d\n",
 							__func__, rc);
 
+	kfree(query_resp);
+	query_resp = NULL;
 	return rc;
 }
 
