b58292c0772d604815a2bf4b523021c2bda15346
Dorimanx <yuri@bynet.co.il>
2017-06-25 18:13:00 +0300

    Uninstaller: set all for N kernel.
    script will uninstall my changes in ramdisk and install stock 4.1.6 kernel.

--- READY-KERNEL/uninstaller/boot/editramdisk.sh
+++ READY-KERNEL/uninstaller/boot/editramdisk.sh
@@ -9,31 +9,29 @@ rm /tmp/boot.img-ramdisk.gz
 
 # remove dorimanx script
 if [ $(grep -c "import /init.dorimanx.rc" /tmp/ramdisk/init.rc) == 1 ]; then
-	sed -i '/import \/init.dorimanx.rc/d" /tmp/ramdisk/init.rc
+	sed -i "/import \/init.dorimanx.rc/d" /tmp/ramdisk/init.rc
 fi
 
 # Remove my modules from ramdisk
-if [ -e /tmp/ramdisk/modules.img ]; then
-	rm /tmp/ramdisk/modules.img
-fi;
+if [ -e /tmp/ramdisk/dori_modules ]; then
+	rm -rf /tmp/ramdisk/dori_modules
+fi
 
-# remove mount modules image and reload modules
-# first cleanup init.qcom.rc
-if [ $(grep -c "rmmod wlan.ko" /tmp/ramdisk/init.qcom.rc) == 1 ]; then
-	sed -i '/rmmod wlan.ko/d' /tmp/ramdisk/init.qcom.rc
+if [ -e /tmp/ramdisk/initd.sh ]; then
+	rm /tmp/ramdisk/initd.sh
 fi
-if [ $(grep -c "rmmod crpl.ko" /tmp/ramdisk/init.qcom.rc) == 1 ]; then
-	sed -i '/rmmod crpl.ko/d' /tmp/ramdisk/init.qcom.rc
+
+if [ -e /tmp/ramdisk/fs_onboot.sh ]; then
+	rm /tmp/ramdisk/fs_onboot.sh
 fi
-if [ $(grep -c "modules.img" /tmp/ramdisk/init.qcom.rc) == 1 ]; then
-	sed -i '/mount ext4 loop\@\/modules\.img \/system\/lib\/modules noatime ro/d' /tmp/ramdisk/init.qcom.rc
+
+if [ -e /tmp/ramdisk/sbin/busybox ]; then
+	rm /tmp/ramdisk/sbin/busybox
 fi
-if [ $(grep -c "insmod /system/lib/modules/crpl.ko" /tmp/ramdisk/init.qcom.rc) == 1 ]; then
-	sed -i '/insmod \/system\/lib\/modules\/crpl\.ko/d' /tmp/ramdisk/init.qcom.rc
-fi
-if [ $(grep -c "insmod /system/lib/modules/wlan.ko" /tmp/ramdisk/init.qcom.rc) == 1 ]; then
-	sed -i '/insmod \/system\/lib\/modules\/wlan\.ko/d' /tmp/ramdisk/init.qcom.rc
-fi
+
+if [ -e /tmp/ramdisk/init.dorimanx.rc ]; then
+	rm /tmp/ramdisk/init.dorimanx.rc
+fi;
 
 # pack ramdisk
 find . | cpio -o -H newc | gzip > /tmp/boot.img-ramdisk.gz
