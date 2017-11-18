04e759e37348bac9f830246d6b25c5723a20979b
Sultanxda <sultanxda@gmail.com>
2017-11-17 20:52:15 -0800

    Squashed revert of buggy and clunky is_keypad_stopped feature:
    commit 8c757969a84ec7592114d5b18cfe0d6008079428
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Fri Nov 17 20:48:58 2017 -0800
    
        Revert "input: synaptics: s1302: Stop keypad when touchscreen is in use"
    
        This implementation is clunky and buggy. We'll grab the touchscreen state
        directly through the s3320 driver instead.
    
        This reverts commit 133e2471f9427b8b56ba5f596f9e8602e3b05bda.
    
    commit b3bad1e9462297a7e86ab81bbb16ef4d33226212
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Fri Nov 17 20:48:14 2017 -0800
    
        Revert "misc: fpc1020: Ignore home key presses when touchscreen is in use"
    
        This reverts commit 9e5003554b79afa1045100c97d2375a752eeb6fe.
    
    commit 8724a168024ba403b7920033986b160bcbc9b275
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Fri Nov 17 20:46:55 2017 -0800
    
        Revert "misc: fpc1020: Fix s1302_is_keypad_stopped() logic for home key"
    
        This reverts commit 2c772344c6ed3a9de5e52384f9455314296db901.
    
    commit 68f68c2048f6534333ffba9329ec6cc0e1d7745a
    Author: Sultanxda <sultanxda@gmail.com>
    Date:   Fri Nov 17 20:46:48 2017 -0800
    
        Revert "input: synaptics: s1302: Make s1302_is_keypad_stopped() safe"
    
        This reverts commit e2fdb3dc4a733dc603b43d64319c06b441627a31.

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -284,7 +284,6 @@ static DEVICE_ATTR(irq, S_IRUSR | S_IWUSR, irq_get, irq_ack);
 extern void int_touch(void);
 extern struct completion key_cm;
 extern bool virtual_key_enable;
-extern bool s1302_is_keypad_stopped(void);
 
 bool key_home_pressed = false;
 EXPORT_SYMBOL(key_home_pressed);
@@ -305,7 +304,7 @@ static ssize_t report_home_set(struct device *dev,
 #endif
         if(virtual_key_enable){
                 key_home_pressed = true;
-        }else if (!s1302_is_keypad_stopped()) {
+        }else{
             input_report_key(fpc1020->input_dev,
                             KEY_HOME, 1);
             input_sync(fpc1020->input_dev);
